03c435b5e1cbd1f859ded7bc51444358085a2fb6
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-04-08 18:01:24 +0700

    Complete

---
+++ src/codegym/Employee.java
@@ -0,0 +1,40 @@
+package codegym;
+
+public class Employee {
+    private String name;
+    private boolean gender;
+    private int[] dateOfBirth;
+
+    public Employee() {
+    }
+
+    public Employee(String name, boolean gender, int[] dateOfBirth) {
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public int[] getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int[] dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
